f04eb8df5dafbb6c490bfc8fdcad67abf82f44d3
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-27 19:53:41 -0500

    creates dynamic search

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-fetch("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5")
-  // we will use ajax to load the information
-  .then(function(response) {
-    // have to convert to JSON
-    return response.json();
-  }) // semicolons stops chaining so don't put it here
-  .then(function(response) {
-    // to show only data
-    showGifs(response.data);
-  });
+var input = document.querySelector('#user-gif-search');
+var searchBtn = document.querySelector('#submit-gif-search');
+var gifContainer = document.querySelector('#gif-result-container');
+
+function findGifs(userInput) {
+  // fetch goes out and gets data from the URL, ajax call, q is the query search
+  fetch("https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=dc6zaTOxFJmzC&limit=15")
+    // we will use ajax to load the information
+    .then(function(response) { // then is a method
+      // response is what we get back from the fetch
+      return response.json(); // converts to JSON so we can easily access objects and properties
+    }) // semicolons stops chaining so don't put it here
+    .then(function(response) {
+      // to show only data and ignore other features
+      showGifs(response.data);
+    });
+} //closes findGifs function
 
   function showGifs(data) {
+    gifContainer.innerHTML = ''; //this clears out gif container if there is any content there
     for (var x = 0; x < data.length; x++) {
       var img = document.createElement('img');
       img.src = data[x].images.original.url;
       // add element to the end of it
-      document.body.appendChild(img);
+      gifContainer.appendChild(img);
     }
   }
+
+  searchBtn.addEventListener('click', function() {
+    var userInput = input.value;
+    if (userInput !== '') {
+      findGifs(userInput);
+    }
+  });
